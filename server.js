d7e06727d7220a5cce12cc030f62e916c28a91cf
Riston Denaux <charlestonshadowsx@gmail.com>
2016-08-02 21:20:25 +0000

    finished timestamp api

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
 var express = require('express'),
-    app = express();
+    app = express(),
+    timestamps={};
     
     app.get('/', function(req, res){
-        res.send("Hello World");
+        res.send("timestamp API server, enter as string params.")
     })
+    
+   app.param('id', function(req, res){
+       var id = req.params.id;
+       
+       if (new Date(id).getTime()>0){
+         var dat = new Date(id)  
+         timestamps.date = dat.toString().substr(0, 16);
+         timestamps.unix = dat.getTime()/1000;
+       } else if (new Date(id*1000).getTime()>0){
+            var dat = (new Date(id*1000)).toGMTString()
+            timestamps.date = dat.substr(0, 16);
+            timestamps.unix = id;
+       }
+       res.send(timestamps);
+   })
+    app.get('/:id', function(req, res){
+       res.end();
+    });
+  
+   
+    
     app.listen(8080, function(){
         console.log("Listenning on 8080")
     })
